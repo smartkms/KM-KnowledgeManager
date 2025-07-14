5aa22b9430ffd8b7b27909a95b1bfba8147b973c
westarman <stefanstrucelj22@gmail.com>
2025-07-14 14:18:32 +0200

    small cleanup

--- knowledge_manager/database/db_search.py
+++ knowledge_manager/database/db_search.py
@@ -10,24 +10,6 @@ def query(text: str):
     return isci_zapise(text)
 
 # using Milvus nomenclature for methods for v2 - search is with embedding, query is with scalar fields
-
-def search_v2(search_str : str, user : str = MILVUS_PUBLIC_USER, type : str | None = None, source :str | None = None) -> List[BaseEntity]:
-    validate_user(user)
-    validate_type(type)
-    validate_source(source)
-    return search_data_v2(query=search_str, type=type, source=source, user=user)
-
-# using Milvus nomenclature for methods for v2 - search is with embedding, query is with scalar fields
-def search_v2(search_str : str, user : str = MILVUS_PUBLIC_USER, type : str | None = None, source :str | None = None) -> List[BaseEntity]:
-    """
-    Searches for entity based on similarity to search_str embedding,
-    filtering result based on user, type, source.
-    """
-    validate_user(user)
-    validate_type(type)
-    validate_source(source)
-    return search_data_v2(query=search_str, type=type, source=source, user=user)
-
 # TODO add number of entities returned
 def search_v2(search_str : str, user : str = MILVUS_PUBLIC_USER, type : str | None = None, source :str | None = None) -> List[BaseEntity]:
     """
